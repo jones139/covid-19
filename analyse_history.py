0375e1191cf41e5166c2651cd792e4a1a68214ea
Graham Jones <grahamjones139@gmail.com>
2020-04-05 10:37:41 +0100

    Added correction for population of local authorities to give better comparison

--- analyse_history.py
+++ analyse_history.py
@@ -6,22 +6,47 @@ import pandas as pd
 latestDataFname = "latestData.xlsx"
 
 authoritiesLst = [
-    "Hartlepool",
-    "Middlesbrough",
-    "Redcar and Cleveland",
-    "Stockton-on-Tees",
-    "Darlington",
-    "County Durham",
+    "E06000001", #"Hartlepool",
+    "E06000002", #"Middlesbrough",
+    "E06000003", #"Redcar and Cleveland",
+    "E06000004", #"Stockton-on-Tees",
+    "E06000005", #"Darlington",
+    "E06000047", #"County Durham",
     #"Bournemouth, Christchurch and Poole"
 ]
 
-def plotAuthorityData(df,authNames,chartFname="chart.png"):
+def getLegendLst(authLst):
+    legendLst = []
+    dfPop = loadPopulationData()
+    for auth in authLst:
+        authName = str(dfPop.query('Code=="%s"' % auth)['Name'].values[0])
+        legendLst.append(authName)
+    return legendLst
+
+def plotAuthorityData(df,authLst,chartFname="chart.png"):
     """ Plots a graph of the data in df for authority authName """
-    print("plotAuthorityData(%s)" % authNames)
-    ax = df.plot(y=authNames)
+    #print("plotAuthorityData(%s)" % authLst)
+    ax = df.plot(y=authLst, grid=True, title="Confirmed Cases - Raw Data")
+    legendLst = getLegendLst(authLst)
+    ax.legend(legendLst)
     ax.figure.savefig(chartFname)
 
-def plotFit(df,authName, chartFname="chart2.png"):
+def plotNormalisedAuthorityData(df, authLst, chartFname="chart2.png"):
+    #print(df.describe(),df.dtypes,df.head())
+    seriesLst = []
+    legendLst = []
+    dfPop = loadPopulationData()
+    #print(dfPop.head())
+    for auth in authLst:
+        seriesLst.append("%s_corr" % auth)
+
+    ax = df.plot(y=seriesLst, grid=True, title="Confirmed Cases - Corrected Data (cases per 100k population)")
+    legendLst = getLegendLst(authLst)
+    ax.legend(legendLst)
+    ax.figure.savefig(chartFname)
+
+
+def plotFit(df,authName, chartFname="chart3.png"):
     """ plots data for a single authority with a simple exponential fit for comparison"""
     print("plotFit - authName=%s" % authName)
     dfSlice = df[authName]
@@ -34,17 +59,38 @@ def loadFile(fname):
     print("loadFile(%s)" % fname)
     df = pd.read_excel(fname, sheet_name='UTLAs', header=7)
     #latestDate=(df.iloc[-1][0]).date()
-    df=df.drop(columns=["Area Code","NHS region", "Region (Governement) "])
-    #print(df.head())
-    #print(df.describe())
-    colNames = list(df.iloc[:,0]) # Authority Names
+    df=df.drop(columns=["Area Name","NHS region", "Region (Governement) "])
+    colNames = list(df.iloc[:,0]) # Authority Codes
     dft = df.transpose()
     dft.columns = colNames
-    dft=dft.drop(index=["Area Name"]) # Drop the first row which is authority names
+    dft=dft.drop(index=["Area Code"]) # Drop the first row which is authority names
+    dft=dft.drop(columns=["Unconfirmed"])
+
+    dfPop = loadPopulationData()
+    # Convert datatypes from 'object' to numeric values.
+    for auth in dft.columns:
+        #dft[auth] = pd.to_numeric(dft[auth])
+        pop = dfPop.query('Code=="%s"' % auth)['All ages']
+        #print("Auth=%s, pop=%.0f," % (auth,pop))
+        corr = float(100000./pop)
+        #print("Auth=%s, pop=%.0f, corr=%f" % (auth,pop,corr))
+        #print(df[auth])
+        dft['%s_corr' % auth] = dft[auth]*corr #.multiply(corr)
+    
     #print(dft.head())
     #print(dft.describe())
 
+    
+
+
     return dft
+
+def loadPopulationData():
+    df = pd.read_excel("populations.xlsx", sheet_name='MYE2-All', header=4)
+    #latestDate=(df.iloc[-1][0]).date()
+    #df=df.drop(columns=["Code"])
+    return df[['Code','Name','All ages']]
+    
 
 if (__name__ == "__main__"):
     print("main()")
@@ -62,7 +108,8 @@ if (__name__ == "__main__"):
 
     df=loadFile(inFname)
 
-    print(df)
+    #print(df)
 
     plotAuthorityData(df,authoritiesLst)
-    plotFit(df,"Hartlepool")
+    plotNormalisedAuthorityData(df,authoritiesLst)
+    #plotFit(df,"Hartlepool")
